faa95cb9d84a59d9ee0ee30d81b9528bfbaf4e0e
skaravai <brizbee0@gmail.com>
2022-04-12 15:31:51 +0300

    added faker

---
+++ src/test/java/guru/qa/tests/RegistrationFormWithFakerTests.java
@@ -0,0 +1,78 @@
+ package guru.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static java.lang.String.format;
+import static utils.RandomUtils.getRandomEmail;
+import static utils.RandomUtils.getRandomString;
+
+public class RegistrationFormWithFakerTests {
+
+    Faker faker = new  Faker();
+
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            mobileNumber = faker.number().digits(10),
+            currentAddress = faker.address().fullAddress(),
+            subject = "English";
+
+
+    String expectedFullName = format("%s %s", firstName, lastName);
+    File file = new File("src/test/resources/img/image.png");
+
+    @BeforeAll
+    static void setUp() {
+    //    Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1250";
+    }
+
+    @Test
+    void fillFormTest() {
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue(mobileNumber);
+
+        // setting date of birth
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("October");
+        $(".react-datepicker__year-select").selectOption("1993");
+        $(".react-datepicker__day--028:not(.react-datepicker__day--outside-month)").click();
+
+
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        //$("#hobbies-checkbox-2").parent().click();
+        $("#uploadPicture").uploadFile(file);
+        $("#currentAddress").setValue(currentAddress);
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        //Assertions
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(expectedFullName), text(email), text(mobileNumber), text(subject), text(currentAddress));
+
+    }
+}
